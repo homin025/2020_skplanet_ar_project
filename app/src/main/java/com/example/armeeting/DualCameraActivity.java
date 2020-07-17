629b2a2a98f27cd8db8f6f329a5c7c7046a2a5d6
homin025 <homin025@users.noreply.github.com>
2020-07-17 16:36:33 +0900

    Add GameFragment/OpenCvFragment

--- app/src/main/java/com/example/armeeting/DualCameraActivity.java
+++ app/src/main/java/com/example/armeeting/DualCameraActivity.java
@@ -1,19 +1,35 @@
 package com.example.armeeting;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-import com.example.armeeting.DualCameraFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class DualCameraActivity extends AppCompatActivity {
+
+    LinearLayout layoutGame;
+    Button buttonRock, buttonScissor, buttonPaper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dualcamera);
+
+        layoutGame = findViewById(R.id.layoutGame1);
+        buttonRock = findViewById(R.id.buttonRock);
+        buttonScissor = findViewById(R.id.buttonScissor);
+        buttonPaper = findViewById(R.id.buttonPaper);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
         if (null == savedInstanceState) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, DualCameraFragment.newInstance())
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_game, GameFragment.newInstance())
+                    .commit();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_opencv, OpenCvFragment.newInstance())
                     .commit();
         }
     }
